18c8d38a93b0e89a9f8bbb5da9f2d28ba26bfd5f
Frank Dillon <dillonf2@outlook.com>
2023-07-13 14:47:35 -0400

    python organizing bears lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',4,'M','brown','happy',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',3,'M','white','lazy',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',6,'F','black','bored',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',2,'M','white','sleepy',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',6,'F','red','happy',FALSE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',4,'M','black','sleepy',TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',8,'F','gray','lazy',FALSE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (Null,4,'M','black','unknown',TRUE)
+
+
+
